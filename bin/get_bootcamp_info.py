07e5c0fe8f23eadcf60b846c3da0e573b33442e1
Greg Wilson <gvwilson@third-bit.com>
2013-08-25 05:56:27 -0400

    Adding more error-checking to bootcamp cache constructor

--- bin/get_bootcamp_info.py
+++ bin/get_bootcamp_info.py
@@ -10,18 +10,18 @@ import requests
 
 GITHUB_IO_TEMPLATE = 'http://swcarpentry.github.io/{0}/'
 
+REQUIRED_KEYS = set('country humandate startdate url venue'.split())
+
 def main(args):
     '''Main driver.'''
 
     reader, writer = setup(args)
     all_urls = yaml.load(reader)
 
-    results = []
-    for url in all_urls:
-        info = fetch(url)
-        info['slug'] = url.split('/')[-1]
-        info['url'] = GITHUB_IO_TEMPLATE.format(info['slug'])
-        results.append(info)
+    results, all_clean = process(all_urls)
+    if not all_clean:
+        print >> sys.stderr, 'Not writing result'
+        sys.exit(1)
 
     yaml.dump(results, writer)
     reader.close()
@@ -37,6 +37,21 @@ def setup(args):
         writer = open(args[1], 'w')
     return reader, writer
 
+def process(all_urls):
+    '''
+    Process URLs, returning list of info structures and flag
+    signalling whether they are all valid.
+    '''
+    results = []
+    all_clean = True
+    for url in all_urls:
+        info = fetch(url)
+        info['slug'] = url.split('/')[-1]
+        info['url'] = GITHUB_IO_TEMPLATE.format(info['slug'])
+        all_clean =  check_info(url, info) and all_clean
+        results.append(info)
+    return results, all_clean
+
 def fetch(url):
     '''Fetch information from a single online repository.'''
     url = url.replace('github.com', 'raw.github.com') + '/gh-pages/index.html'
@@ -44,6 +59,15 @@ def fetch(url):
     header = response.text.split('---')[1]
     info = yaml.load(header)
     return info
+
+def check_info(url, info):
+    '''Check that all required information for bootcamp is present.'''
+    missing = REQUIRED_KEYS - set(info.keys())
+    if missing:
+        missing = sorted(list(missing))
+        print >> sys.stderr, 'Info for {0} missing key(s): {1}'.format(url, missing)
+        return False
+    return True
 
 if __name__ == '__main__':
     main(sys.argv[1:])
