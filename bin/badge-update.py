2db64a4a8b0a41d8fdf26c9baf2dc297370fbec2
Raniere Silva <raniere@ime.unicamp.br>
2013-12-04 14:00:39 -0200

    Update `bin/badge-update.py`
    Thanks to @gvwilson to the comments.
    
    Fixes:
    
    * Check if the badges had been update previously.

--- bin/badge-update.py
+++ bin/badge-update.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-'''Update a JSON blob for a badge from v0.5.0 to v1.0.0. Change information can
+"""Update a JSON blob for a badge from v0.5.0 to v1.0.0. Change information can
 be found at
-https://github.com/mozilla/openbadges/wiki/Assertion-Specification-Changes.'''
+https://github.com/mozilla/openbadges/wiki/Assertion-Specification-Changes."""
 
 import sys
 import os
@@ -10,27 +10,31 @@ import datetime
 import hashlib
 import json
 
-def main(fbadge, write, nobackups):
-    '''Main program driver.'''
-    badge = read_old(fbadge)
-    badge['file-name'] = fbadge  # We need the name of the file.
-    badge = create_badge_assertion(badge)
-    output = json.dumps(badge, indent=True, sort_keys=True)
-    if not write and not nobackups:
-        print(output)
-    else:
-        if write:
-            shutil.copyfile(fbadge, '{}.bak'.format(fbadge))
-        with open(fbadge, 'w') as f:
-            f.write(output)
+SwCarpentryURL = "http://software-carpentry.org"
 
-def read_old(fbadge):
+def main(badge_filename, save_result, backups):
+    """Main program driver."""
+    badge = read_old(badge_filename)
+
+    # Check if badge is from the older API
+    if type(badge["badge"]) is dict:
+        badge = update_badge_assertion(badge, badge_filename)
+        output = json.dumps(badge, indent=True, sort_keys=True)
+        if not save_result and not backups:
+            print(output)
+        else:
+            if backups:
+                shutil.copyfile(badge_filename, "{}.bak".format(badge_filename))
+            with open(badge_filename, "w") as f:
+                f.write(output)
+
+def read_old(badge_filename):
     """Read JSON with old badge."""
-    with open(fbadge, 'r') as f:
+    with open(badge_filename, "r") as f:
         badge = json.load(f)
     return badge
 
-def create_badge_assertion(old_badge):
+def update_badge_assertion(old_badge, badge_filename):
     """Create a BadgeAssertion based on the old badge structure.
 
     For more information about BadgeAssertion:
@@ -38,81 +42,63 @@ def create_badge_assertion(old_badge):
 
     NOTE: for non-mandatory fields we use a `try` block.
     """
-    new_badge = dict()
+    new_badge = {
+        "uid":old_badge["recipient"],
+        "recipient":{
+            "identity":old_badge["recipient"],
+            "type":"email",
+            "hashed":True},
+        "badge":set_badge(old_badge["badge"]["name"]),
+        "verify":{
+            "type":"hosted",
+            "url":"{0}/{1}".format(SwCarpentryURL, badge_filename)}}
 
-    new_badge['uid'] = old_badge['recipient']
+    if "salt" in old_badge.keys():
+        new_badge["recipient"]["salt"] = old_badge["salt"]
+    if "issued_on" in old_badge.keys():
+        new_badge["issuedOn"] = old_badge["issued_on"]
 
-    new_badge['recipient'] = dict()
-    new_badge['recipient']['identity'] = old_badge['recipient']
-    new_badge['recipient']['type'] = 'hosted'
-    new_badge['recipient']['hashed'] = True
-    try:
-        new_badge['recipient']['salt'] = old_badge['salt']
-    except:
-        pass
-
-    if old_badge['badge']['name'] == 'Creator':
-        new_badge['badge'] = 'http://software-carpentry.org/badges/class/creator.json'
-    elif old_badge['badge']['name'] == 'Instructor':
-        new_badge['badge'] = 'http://software-carpentry.org/badges/class/instructor.json'
-    elif old_badge['badge']['name'] == 'Organizer':
-        new_badge['badge'] = 'http://software-carpentry.org/badges/class/organizer.json'
-    else:
-        raise ValueError('Can\'t match badge.name field.')
-
-    new_badge['verify'] = dict()
-    new_badge['verify']['type'] = 'hosted'
-    new_badge['verify']['url'] = 'http://software-carpentry.org/{0}'.format(
-            old_badge['file-name'])
-
-    try:
-        new_badge['issuedOn'] = old_badge['issued_on']
-    except:
-        pass
-
-    try:
+    if "badge" in old_badge.keys() and "image" in old_badge["badge"].keys():
         # Check for relative path
-        if old_badge['badge']['image'][0] == '/':
-            new_badge['image'] = 'http://software-carpentry.org{0}'.format(
-                    old_badge['badge']['image'])
+        if old_badge["badge"]["image"][0] == "/":
+            new_badge["image"] = "{0}{1}".format(
+                    SwCarpentryURL, old_badge["badge"]["image"])
         else:
-            new_badge['image'] = old_badge['badge']['image']
-    except:
-        if old_badge['badge']['name'] == 'Creator':
-            new_badge['image'] = 'http://software-carpentry.org/img/badges/creator.png'
-        elif old_badge['badge']['name'] == 'Instructor':
-            new_badge['image'] = 'http://software-carpentry.org/img/badges/instructor.png'
-        elif old_badge['badge']['name'] == 'Organizer':
-            new_badge['badge'] = 'http://software-carpentry.org/badges/class/organizer.json'
-        else:
-            raise ValueError('Can\'t match badge.name field.')
+            new_badge["image"] = old_badge["badge"]["image"]
+    else:
+        new_badge["image"] = set_badge_image(old_badge["badge"]["name"])
 
-    try:
-        new_badge['evidence'] = old_badge['evidence']
-    except:
-        pass
+    if "evidence" in old_badge.keys():
+        new_badge["evidence"] = old_badge["evidence"]
 
-    try:
-        new_badge['expires'] = old_badge['expires']
-    except:
-        pass
-
+    if "expires" in old_badge.keys():
+        new_badge["expires"] = old_badge["expires"]
 
     return new_badge
 
-if __name__ == '__main__':
+def set_badge(name):
+    assert name in ['Creator', 'Instructor', 'Organizer'], "Can't match badge.name field."
+    return "{0}/badges/class/{1}.json".format(
+            SwCarpentryURL, name.lower())
+
+def set_badge_image(name):
+    assert name in ['Creator', 'Instructor', 'Organizer'], "Can't match badge.name field."
+    return "{0}/img/badges/{1}.json".format(
+            SwCarpentryURL, name.lower())
+
+if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(
-            description='A python script to update a JSON blob for a badge from v0.5.0 to v1.0.0')
-    parser.add_argument('-w', '--write', action='store_true',
-            help='Write back modified badge')
-    parser.add_argument('-n', '--nobackups', action='store_true',
-            help='Don\'t write backups for modified badges')
-    parser.add_argument('badge', nargs='+',
-            help='The name of the JSON blob to update')
+            description="A python script to update a JSON blob for a badge from v0.5.0 to v1.0.0")
+    parser.add_argument("-w", "--write", action="store_true",
+            help="Write back modified badge")
+    parser.add_argument("-b", "--backups", action="store_true",
+            help="Write backups for modified badges")
+    parser.add_argument("badge", nargs="+",
+            help="The name of the JSON blob to update")
 
     args = parser.parse_args()
 
     for b in args.badge:
-        main(b, args.write, args.nobackups)
+        main(b, args.write, args.backups)
