98f515ce3ed725e8b01c4d73c57796d915f064e9
Mike Jackson <mikezonk@gmail.com>
2014-01-28 15:14:21 +0000

    Cleaned up make_bootcamp_rss_feed.py - removed commented out code

--- bin/make_bootcamp_rss_feed.py
+++ bin/make_bootcamp_rss_feed.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 '''
-Compile the Software Carpentry calendar file.
+Create bootcamp-feed.xml for RSS feed for Software Carpentry 
+boot camps.
 '''
 
-import sys
-import os
-import yaml
 import datetime
+import os
+import sys
 import re
-from PyRSS2Gen import RSS2, RSSItem
+import yaml
 from optparse import OptionParser
+from PyRSS2Gen import RSS2, RSSItem, Guid
 from make_rss_feed import ContentEncodedRSS2, ContentEncodedRSSItem
 try:  # Python 3
     from urllib.parse import urlparse
@@ -20,30 +21,25 @@ from util import CONFIG_YML, STANDARD_YML, load_info
 #----------------------------------------
 
 def main():
-    '''Main driver for calendar regeneration.'''
+    '''Main driver for boot camp feed regeneration.'''
 
     options, args = parse_args()
     config = load_info(os.curdir)
     config['site'] = options.site
-    text_file = os.path.join(options.output, 'bootcamps.txt')
 
-
-#    selection = config['blog'][-config['recent_length']:]
-#    selection = config['blog'][-config['recent_length']:]
-#    selection.reverse()
-#    build_blog_rss(config,
-#                   os.path.join(options.output, 'bootcamp-feed.xml'),
-#                   selection)
     build_bootcamp_rss(config,
                        os.path.join(options.output, 'bootcamp-feed.xml'))
-
-#    bcw = BootCampWriter()
-#    bcw(text_file, config)
 
     # TODO - TEST TO UPDATE BOOTCAMP_URLS
     # TODO - SORT INTO DATE ORDER
     # TODO - CLEAN UP RSS FEED CONTENT
     # TODO - PARSE FEED FOR COMPLIANCE
+    # TODO - resolve country
+    # TODO - resolve pubDate w.r.t specification
+    # TODO - replace title and description
+    # http://cyber.law.harvard.edu/rss/rss.html
+    # http://www.w3schools.com/rss/rss_item.asp
+    # python bin/make_bootcamps.py -o _site/ -s _site/
 
 #----------------------------------------
 
@@ -62,27 +58,35 @@ def parse_args():
 
 def build_bootcamp_rss(config, filename):
     '''
-    Generate RSS2 feed.xml file for bootcamps given the metadata blobs for
+    Generate RSS2 file for bootcamps given the metadata blobs for
     recent bootcamps.
     '''
-
     items = []
     site = config['site']
     for bc in config['bootcamps']:
         if bc['country'] == 'United-Kingdom':
-#            print bc
-#            lines.extend(self.bootcamp(config['site'], config['timestamp'], bc))
             # Create RSS items.
+            at = ''
+            if bc.get('address'):
+                at = 'at {0}'.format(bc['address'])
+            instructors = ''
+            if bc.get('instructor'):
+                instructors='led by {0}'.format(', '.join(bc['instructor']))
+            description = 'A boot camp will be held on {0} {1} {2}'.format(bc['humandate'], at, instructors)
+            guid = Guid('{0}/{1}'.format(site,bc['slug']), isPermaLink=False)
             items.append(ContentEncodedRSSItem(title=bc['venue'],
-                                           creator=bc['contact'],
-                                           link=bc['url'],
-                                           description=', '.join(bc['instructor']),
-                                           content=bc['country'],
-                                           pubDate=datetime.datetime.fromordinal(bc['startdate'].toordinal())))
+#                                               creator=bc['contact'],
+                                               author=bc['contact'],
+                                               guid=guid,
+                                               link=bc['url'],
+                                               description=description,
+                                               content=bc['country'],
+#                                               category=bc['country'],
+                                               pubDate=datetime.datetime.fromordinal(bc['startdate'].toordinal())))
 
     # Create RSS feed.
-    rss = ContentEncodedRSS2(title='Boot camps',
-                             description='Boot camps',
+    rss = ContentEncodedRSS2(title='Software Carpentry boot camps',
+                             description='Helping researchers do more, in less time, with less pain',
                              lastBuildDate=datetime.datetime.utcnow(),
                              link=site,
                              items=items)
@@ -90,74 +94,6 @@ def build_bootcamp_rss(config, filename):
     # Save.
     with open(filename, 'w') as writer:
         rss.write_xml(writer)
-
-
-class BootCampWriter(object):
-    '''
-    Dump information on a boot camp.
-    '''
-    def __call__(self, filename, config):
-        lines = ['BEGIN',]
-        for bc in config['bootcamps']:
-            if bc['country'] == 'United-Kingdom':
-#                print bc
-                lines.extend(self.bootcamp(config['site'], config['timestamp'], bc))
-        lines.extend(['END', ''])
-        content = '\r\n'.join(lines)
-        # From RFC 5545, section 3.1.4 (Character Set):
-        # The default charset for an iCalendar stream is UTF-8.
-        with open(filename, 'wb') as writer:
-            writer.write(content.encode('utf-8'))
-
-# python bin/make_bootcamps.py -o _site/ -s _site/
-### 'country': 'United-Kingdom', 
-### 'url': 'http://apawlik.github.io/2014-01-14-manchester/', 
-### 'venue': 'University of Manchester', 
-# 'instructor': ['Michael Croucher', 'Jos Martin', 'Shoaib Sufi', 'Aleksandra Pawlik'], 
-# 'humandate': 'Jan 14-15, 2014', 
-# 'contact': 'admin-uk@software-carpentry.org', 
-# ---
-# 'latlng': '53.467972,-2.233154', 
-# 'address': 'Room IT407, Information Technology Building, University of Manchester, Oxford Road, M13 9PL', 
-# 'user': 'apawlik', 'registration': 'restricted', 
-# 'layout': 'bootcamp', 
-# 'slug': '2014-01-14-manchester', 
-# 'startdate': datetime.date(2014, 1, 14), 
-# 'enddate': datetime.date(2014, 1, 15), 
-# 'humantime': '9:00 am - 5:00 pm', 
-# 'root': '.'}
-
-    def bootcamp(self, site, timestamp, info):
-        uid = '{0}@{1}'.format(info['slug'],
-                               urlparse(site).netloc or 'software-carpentry.org')
-        if 'enddate' in info:
-            end = info['enddate']
-        else:  # one day boot camp?
-            end = info['startdate']
-        end + datetime.timedelta(1)  # non-inclusive end date
-        lines = [
-            'BOOTCAMP-BEGIN',
-#            'UID:{0}'.format(uid),
-#            'DTSTAMP:{0}'.format(timestamp),
-            'DATE:{0}'.format(info['humandate']),
-            'CONTACT:{0}'.format(info['contact']),
-            'URL:{0}'.format(info['url']),
-            'COUNTRY:{0}'.format(info['country']),
-            'VENUE:{0}'.format(info['venue']),
-            'INSTRUCTOR:{0}'.format(', '.join(info['instructor'])),
-        ]
-        if info.get('latlng'):
-            latlng = re.sub(r'\s+', '', info['latlng']).replace(',', ';')
-            lines.append('GEO:{0}'.format(latlng))
-        lines.append('BOOTCAMP-END')
-        return lines
-
-    def escape(self, value):
-        '''Escape text following RFC 5545.'''
-        for char in ['\\', ';', ',']:
-            value = value.replace(char, '\\' + char)
-        value.replace('\n', '\\n')
-        return value
 
 #----------------------------------------
 
