1e45a96de207d53be6eecc8db05281d240b73dab
Greg Wilson <gvwilson@third-bit.com>
2015-01-05 07:55:53 -0800

    1.  Making sure the site can be built without a GitHub API token. 2.  Updating instructions.

--- bin/make-dashboard.py
+++ bin/make-dashboard.py
@@ -4,10 +4,9 @@
 
 import sys
 import yaml
-from github import Github
 from util import DASHBOARD_CACHE
 
-controls = (
+CONTROLS = (
     ('swcarpentry/shell-novice', 'Introduction to the Unix shell'),
     ('swcarpentry/git-novice', 'Introduction to Git'),
     ('swcarpentry/hg-novice', 'Introduction to Mercurial'),
@@ -23,36 +22,52 @@ controls = (
     ('swcarpentry/site', 'Software Carpentry website'),
 )
 
-assert len(sys.argv) == 3, 'Usage: make-dashboard.py token-file output-file'
-token_file = sys.argv[1]
-output_file = sys.argv[2]
+def get_connection(token_file):
+    '''Get a connection to GitHub if the library and token file are available.'''
+    try:
+        from github import Github
+        with open(token_file, 'r') as reader:
+            token = reader.read().strip()
+        cnx = Github(token)
+    except:
+        cnx = None
+    return cnx
 
-with open(token_file, 'r') as reader:
-    token = reader.read().strip()
+def process(cnx):
+    '''Gather information.'''
+    if not cnx:
+        return []
+    all_records = []
+    dashboard = {
+        'records' : all_records,
+        'num_repos' : 0,
+        'num_issues' : 0
+    }
+    for (ident, description) in CONTROLS:
+        print '+', ident
+        dashboard['num_repos'] += 1
+        r = cnx.get_repo(ident)
+        record = {'ident' : ident,
+                  'description' : description,
+                  'url' : str(r.html_url),
+                  'issues' : []}
+        all_records.append(record)
+        for i in r.get_issues(state='open'):
+            record['issues'].append({'number' : i.number,
+                                     'title' : str(i.title),
+                                     'url' : str(i.html_url),
+                                     'updated' : i.updated_at.strftime('%Y-%m-%d')})
+            dashboard['num_issues'] += 1
+        record['issues'].sort(lambda x, y: - cmp(x['updated'], y['updated']))
 
-g = Github(token)
-all_records = []
-dashboard = {
-    'records' : all_records,
-    'num_repos' : 0,
-    'num_issues' : 0
-}
-for (ident, description) in controls:
-    print '+', ident
-    dashboard['num_repos'] += 1
-    r = g.get_repo(ident)
-    record = {'ident' : ident,
-              'description' : description,
-              'url' : str(r.html_url),
-              'issues' : []}
-    all_records.append(record)
-    for i in r.get_issues(state='open'):
-        record['issues'].append({'number' : i.number,
-                                 'title' : str(i.title),
-                                 'url' : str(i.html_url),
-                                 'updated' : i.updated_at.strftime('%Y-%m-%d')})
-        dashboard['num_issues'] += 1
-    record['issues'].sort(lambda x, y: - cmp(x['updated'], y['updated']))
+def main():
+    '''Main driver.'''
+    token_file = sys.argv[1]
+    output_file = sys.argv[2]
+    cnx = get_connection(token_file)
+    dashboard = process(cnx)
+    with open(output_file, 'w') as writer:
+        yaml.dump(dashboard, writer, encoding='utf-8', allow_unicode=True)
 
-with open(output_file, 'w') as writer:
-    yaml.dump(dashboard, writer, encoding='utf-8', allow_unicode=True)
+if __name__ == '__main__':
+    main()
