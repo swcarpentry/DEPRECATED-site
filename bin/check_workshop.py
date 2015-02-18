059c7ce1695cb53a112100989c11fdfadc63eabf
Greg Wilson <gvwilson@third-bit.com>
2015-02-17 21:02:16 -0500

    More workshops and improvements to the checker

--- bin/check_workshop.py
+++ bin/check_workshop.py
@@ -1,12 +1,17 @@
 import sys
+import re
 from get_workshop_info import fetch, check_info
 
 assert len(sys.argv) == 2, 'Usage: check_workshop.py url'
 url = sys.argv[1]
 
+m = re.match(r'^https?://(.+)\.github\.io/([^/]+)/?$', url)
+if m:
+    url = 'https://github.com/{0}/{1}'.format(m.group(1), m.group(2))
+
 info = fetch(url)
 info['url'] = url
 if check_info(url, info):
-    print 'OK'
+    print url
 else:
-    print 'failure'
+    print 'FAIL:', url
