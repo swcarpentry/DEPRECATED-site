758c96b7544675ab859f81ac3d31d6c157b9603f
Greg Wilson <gvwilson@third-bit.com>
2015-05-16 17:40:26 -0400

    Updating badges

---
+++ bin/list-badge-dates.py
@@ -0,0 +1,26 @@
+import sys
+import json
+
+USAGE = 'Usage: {0} [--start date] filenames...'.format(sys.argv[0])
+
+filenames = sys.argv[1:]
+if not filenames:
+    print >> sys.stderr, USAGE
+    sys.exit(1)
+
+start = None
+if filenames[0] == '--start':
+    if len(filenames) <= 1:
+        print >> sys.stderr, USAGE
+        sys.exit(1)
+    start = filenames[1]
+    filenames = filenames[2:]
+
+for f in filenames:
+    with open(f, 'r') as reader:
+        data = json.load(reader)
+        if 'issuedOn' in data:
+            if (start is None) or (data['issuedOn'] >= start):
+                print '{0}: {1}'.format(f, data['issuedOn'])
+        else:
+            print >> sys.stderr, 'No issuedOn in "{0}"'.format(f)
