e9dd4d26b8802891748f0299544049b8e4d6e678
Greg Wilson <gvwilson@third-bit.com>
2015-06-12 16:13:17 -0400

    More instructor badges

--- bin/badge-create-multi.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Usage: badge-create-multi.sh user.name user.name
-for i in $*
-do
-    python bin/badge-create.py $(sqlite3 ~/s/admin/roster.db 'select person || " " || email || " instructor" from person where person="'$i'";')
-done
