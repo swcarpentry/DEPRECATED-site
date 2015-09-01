b4f31085af2e2f8e1d00ae82659438cda11ae3b9
Greg Wilson <gvwilson@third-bit.com>
2015-09-01 21:39:07 +0100

    Making list-urls.py work with _config.yml

--- bin/list-urls.py
+++ bin/list-urls.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-'''Get workshop URLs from cached workshop info.'''
+'''Get workshop URLs from _config.yml.'''
 import sys
 import yaml
 
 def main():
     '''Main driver.'''
-    workshops = yaml.load(sys.stdin)
-    for w in workshops:
+    config = yaml.load(sys.stdin)
+    for w in config['workshops']:
         print(w['slug'], w['url'])
 
 if __name__ == '__main__':
